901fd9cca843f0261e3d5e24a857d9f8a2b1f4c7
Tordenskiold <niklas.overby@gmail.com>
2019-10-22 14:14:12 +0200

    Cleaned up layout of files

--- IT2805   -   Web Technologies/Assignments/Assignment 5/Delivery/script.js
+++ IT2805   -   Web Technologies/Assignments/Assignment 5/Delivery/script.js
@@ -1,4 +1,5 @@
 /* Part 2 */
+
 console.log('PART 2')
 
 for (var i = 1; i <= 20; i++)
@@ -7,6 +8,7 @@ for (var i = 1; i <= 20; i++)
 
 
 /* Part 3 */
+
 console.log('PART 3')
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
@@ -31,6 +33,7 @@ document.getElementById('title').innerText = "Hello JavaScript";
 
 
 /* Part 5 */
+
 function changeDisplay () {
     document.getElementById("magic").style.display = "none";
 }
@@ -50,6 +53,7 @@ function reset () {
 
 
 /* Part 6 */
+
 const technologies = [
     'HTML5',
     'CSS3',
